0fcc948703a2fa3553ff402f51f536faa0a7d6be
JanushR <janush.r@s.amity.edu>
2025-05-17 19:11:30 +0530

    b

---
+++ hello..c
@@ -0,0 +1,51 @@
+    // Calculate sum
+    sum = a + b;
+
+    // Display the result
+    printf("Sum of %d and %d is %d\n", a, b, sum);
+        // Check if the sum is even or odd
+    if (sum % 2 == 0) {
+        printf("The sum is even.\n");
+    } else {
+        printf("The sum is odd.\n");
+    }
+
+    return 0;
+}
+
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+hello..c[+] [unix] (05:29 01/01/1970)                                                                                                     6,1 All
+-- INSERT --
+
