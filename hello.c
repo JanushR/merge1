854445b4ac99a5eac3587299f51e6e772aa1638b
JanushR <janush.r@s.amity.edu>
2025-05-17 19:07:54 +0530

    a

---
+++ hello.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main() {
+    int a, b, sum;
+
+    // Ask user for input
+    printf("Enter first number: ");
+    scanf("%d", &a);
+
+    printf("Enter second number: ");
+    scanf("%d", &b);
+
